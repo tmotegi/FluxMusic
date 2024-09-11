76e8c82c2989961a6a24ee1532eede6f370ce3a2
tmotegi <35474311+tmotegi@users.noreply.github.com>
2024-09-11 23:36:57 +0900

    feat: use version instead of local path

--- modules/conditioner.py
+++ modules/conditioner.py
@@ -11,19 +11,19 @@ class HFEmbedder(nn.Module):
         self.output_key = "last_hidden_state" if self.is_t5 else "pooler_output"
 
         if version.startswith("openai"): 
-            local_path = '/maindata/data/shared/multimodal/public/ckpts/stable-diffusion-3-medium-diffusers/text_encoder' 
-            local_path_tokenizer = '/maindata/data/shared/multimodal/public/ckpts/stable-diffusion-3-medium-diffusers/tokenizer' 
-            self.tokenizer: CLIPTokenizer = CLIPTokenizer.from_pretrained(local_path_tokenizer, max_length=max_length)
-            self.hf_module: CLIPTextModel = CLIPTextModel.from_pretrained(local_path, **hf_kwargs).half()
+            # local_path = '/maindata/data/shared/multimodal/public/ckpts/stable-diffusion-3-medium-diffusers/text_encoder'
+            # local_path_tokenizer = '/maindata/data/shared/multimodal/public/ckpts/stable-diffusion-3-medium-diffusers/tokenizer'
+            self.tokenizer: CLIPTokenizer = CLIPTokenizer.from_pretrained(version, max_length=max_length)
+            self.hf_module: CLIPTextModel = CLIPTextModel.from_pretrained(version, **hf_kwargs).half()
         elif version.startswith("laion"): 
-            local_path = '/maindata/data/shared/multimodal/public/dataset_music/clap'
-            self.tokenizer = AutoTokenizer.from_pretrained(local_path, max_length=max_length)
-            self.hf_module: ClapTextModel = ClapTextModel.from_pretrained(local_path, **hf_kwargs).half()
+            # local_path = '/maindata/data/shared/multimodal/public/dataset_music/clap'
+            self.tokenizer = AutoTokenizer.from_pretrained(version, max_length=max_length)
+            self.hf_module: ClapTextModel = ClapTextModel.from_pretrained(version, **hf_kwargs).half()
         else: 
-            local_path = '/maindata/data/shared/multimodal/public/ckpts/stable-diffusion-3-medium-diffusers/text_encoder_3' 
-            local_path_tokenizer = '/maindata/data/shared/multimodal/public/ckpts/stable-diffusion-3-medium-diffusers/tokenizer_3' 
-            self.tokenizer: T5Tokenizer = T5Tokenizer.from_pretrained(local_path_tokenizer, max_length=max_length)
-            self.hf_module: T5EncoderModel = T5EncoderModel.from_pretrained(local_path, **hf_kwargs).half()
+            # local_path = '/maindata/data/shared/multimodal/public/ckpts/stable-diffusion-3-medium-diffusers/text_encoder_3'
+            # local_path_tokenizer = '/maindata/data/shared/multimodal/public/ckpts/stable-diffusion-3-medium-diffusers/tokenizer_3'
+            self.tokenizer: T5Tokenizer = T5Tokenizer.from_pretrained(version, max_length=max_length)
+            self.hf_module: T5EncoderModel = T5EncoderModel.from_pretrained(version, **hf_kwargs).half()
 
         self.hf_module = self.hf_module.eval().requires_grad_(False)
 
